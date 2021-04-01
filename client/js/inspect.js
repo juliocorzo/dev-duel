4b378dc56f73e93316553d8bf5a199673caae947
Julio Corzo <juliocorzo@me.com>
2021-04-01 09:50:18 -0700

    Implemented inspect.js, missing error handling.

--- client/js/inspect.js
+++ client/js/inspect.js
@@ -15,6 +15,37 @@ $('form').submit(() => {
         Attach the data returned to the DOM
         The data currently hard-coded into the DOM is placeholder data
        */
+        $('.username').html(data.username)
+        $('.full-name').html(data.name)
+        $('.location').html(data.location)
+        $('.email').html(data.email)
+        $('.bio').html(data.bio)
+        $('.avatar').attr("src", data.avatar_url)
+
+        let titlesString = ''
+        if(data.titles.length < 1) {
+            titlesString += 'User has no titles.'
+            $('.titles').html(titlesString)
+        }
+        else {
+            for(let x = 0; x < data.titles.length; x++) {
+                if(x === 0) {
+                    titlesString += `${data.titles[x]}`
+                } else {
+                    titlesString += `, ${data.titles[x]}`
+                }
+            }
+            $('.titles').html(titlesString)
+        }
+
+        $('.favorite-language').html(data.favorite_language)
+        $('.total-stars').html(data.total_stars)
+        $('.most-starred').html(data.highest_starred)
+        $('.public-repos').html(data.public_repos)
+        $('.perfect-repos').html(data.perfect_repos)
+        $('.followers').html(data.followers)
+        $('.following').html(data.following)
+
 
       $('.user-results').removeClass('hide') // Display '.user-results' element
     })
